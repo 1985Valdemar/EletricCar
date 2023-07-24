0896fb0cf5f9b0c0c2568cce3177d6540e095384
1985Valdemar <vteider@yahoo.com>
2023-07-24 13:49:31 -0300

    Atualizacao calculadora

--- app/src/main/java/com/nomeempresa/eletriccar/MainActivity.kt
+++ app/src/main/java/com/nomeempresa/eletriccar/MainActivity.kt
@@ -1,44 +1,44 @@
-package com.nomeempresa.eletriccarapp
+package com.nomeempresa.eletriccar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
-    lateinit var preço:EditText
+    lateinit var preco: EditText
     lateinit var btnCalcular: Button
-    lateinit var kmPercorrido:EditText
+    lateinit var kmPercorrido: EditText
     lateinit var resultado: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
         setContentView(R.layout.activity_main)
         setupView()
         setupListeners()
     }
 
-    fun setupView(){
-        preço = findViewById(R.id.et_preço_kWh)
+    fun setupView() {
+        preco = findViewById(R.id.et_preço_kWh)
         kmPercorrido = findViewById(R.id.et_km_percorrido)
         resultado = findViewById(R.id.tv_Resultado)
         btnCalcular = findViewById(R.id.btn_calcular)
 
     }
 
-    fun setupListeners(){
+    fun setupListeners() {
         btnCalcular.setOnClickListener {
             calcular()
         }
     }
-    fun calcular (){
-        val preço = preço.text.toString().toFloat()
+
+    fun calcular() {
+        val preco = preco.text.toString().toFloat()
         val km = kmPercorrido.text.toString().toFloat()
 
-        val result = preço / km
+        val result = preco / km
 
         resultado.text = result.toString()
 
